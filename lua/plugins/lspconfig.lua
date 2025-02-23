f4d413797d62b5e4ac891c048243a29573994dde
Brian Vess <brian.vess@gmail.com>
2025-02-23 08:07:28 -0500

    auto install of zls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,18 +7,22 @@ return {
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
+		local lsp_capabilities = require("blink.cmp").get_lsp_capabilities()
+
 		local default_setup = function(server)
-			local lsp_capabilities = require("blink.cmp").get_lsp_capabilities()
 			lspconfig[server].setup({
 				capabilities = lsp_capabilities,
 			})
 		end
 
-		require("lspconfig")["zls"].setup({})
+		-- Only needed if using a dev version of zls
+		-- require("lspconfig")["zls"].setup({
+		-- 	capabilities = lsp_capabilities,
+		-- })
 
 		require("mason").setup({})
 		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "gopls", "clangd" },
+			ensure_installed = { "lua_ls", "gopls", "clangd", "zls" },
 			automatic_installation = false,
 			handlers = {
 				default_setup,
